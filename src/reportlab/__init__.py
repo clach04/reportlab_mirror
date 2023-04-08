2d32aaa6ff94fef95765e9ca55c00e5f2703fb7b
clach04 <clach04@gmail.com>
2023-04-08 10:12:44 -0700

    Correct reportlab version number (not picked up from source code control)

--- src/reportlab/__init__.py
+++ src/reportlab/__init__.py
@@ -1,7 +1,7 @@
 #Copyright ReportLab Europe Ltd. 2000-2012
 #see license.txt for license details
 #history http://www.reportlab.co.uk/cgi-bin/viewcvs.cgi/public/reportlab/trunk/reportlab/__init__.py
-__version__=''' $Id$ '''
+__version__='''3.0.0'''
 __doc__="""The Reportlab PDF generation library."""
 Version = "3.0"
 
